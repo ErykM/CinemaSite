d2f62001520d7e06033dd35e9b5233ec17c3761d
eryk <eryk.malczyk@bitcomp.fi>
2020-12-20 19:00:08 +0100

    Fix spelling error in Movie model

--- cinemaSite/api/models.py
+++ cinemaSite/api/models.py
@@ -15,7 +15,7 @@ class DecimalRangeField(models.DecimalField):
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
-    ticket_prize = models.DecimalField(decimal_places=2, max_digits=5)
+    ticket_price = models.DecimalField(decimal_places=2, max_digits=5)
     rating = DecimalRangeField(min_value=0.0, max_value=10.0, decimal_places=2, max_digits=4)
 
     def __str__(self):
