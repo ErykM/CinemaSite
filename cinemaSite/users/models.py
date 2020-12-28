3e081052a8fdc4b772e951f23bb5167a7ec182d7
eryk <eryk.malczyk@bitcomp.fi>
2020-12-28 16:14:10 +0100

    Add CustomUser and CustomUserManager models

--- cinemaSite/users/models.py
+++ cinemaSite/users/models.py
@@ -1,7 +1,41 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
-class RestUser:
+def create_superuser(self, email, name, password, **kwargs):
+    kwargs.setdefault('is_super', True)
 
-    email = models.EmailField(gettext_lazy('email address'), unique=True)
+    if kwargs.get('is_super') is not True:
+        raise ValueError(
+            'Superuser must have flag is_super assigned to True'
+        )
+    return self.create_superuser(email, name, password, **kwargs)
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_user(self, email, name, password, **kwargs):
+        if not email:
+            raise ValueError('You must provide email address')
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, name=name, **kwargs)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.CharField(max_length=150, unique=True)
     name = models.CharField(max_length=150, unique=True)
+    password_hash = models.CharField(max_length=100)
+    is_super = models.BooleanField(default=False)
+
+    USERNAME_FIELD = email
+    REQUIRED_FIELDS = [email]
+
+    def __str__(self):
+        return self.name
