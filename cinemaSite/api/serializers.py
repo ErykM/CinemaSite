5859b5760513a3a4741dba6621317378bd0545c1
eryk <eryk.malczyk@bitcomp.fi>
2021-01-02 20:37:06 +0100

    Implement custom list method for TicketsViewSet

--- cinemaSite/api/serializers.py
+++ cinemaSite/api/serializers.py
@@ -1,5 +1,3 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import Movie, Ticket
@@ -8,10 +6,18 @@ from .models import Movie, Ticket
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Movie
+        fields = ('title', 'ticket_price')
+
+
+class MovieByIdSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Movie
         fields = ('id', 'title', 'description', 'ticket_price', 'rating')
 
 
 class TicketSerializer(serializers.HyperlinkedModelSerializer):
+    movie_title = serializers.CharField(source='movie.title')
+
     class Meta:
         model = Ticket
-        fields = ('id', )
+        fields = ('id', 'movie_title')
--- cinemaSite/api/views.py
+++ cinemaSite/api/views.py
@@ -1,17 +1,36 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from .serializers import MovieSerializer, TicketSerializer
+from users.models import CustomUser
+from .serializers import MovieSerializer, TicketSerializer, MovieByIdSerializer
 from .models import Movie, Ticket
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return MovieByIdSerializer
+        return MovieSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        payload = request.auth.payload
+        user_id = payload.get('user_id')
+
+        queryset = Ticket.objects.filter(user__id=user_id)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
